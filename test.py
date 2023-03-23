e883a35bdaeb1d88b7c2101c9ced1e899a2ec577
kondrich <123kondrich@gmail.com>
2023-03-22 20:39:11 -0700

    OOP

--- test.py
+++ test.py
@@ -1,10 +1,38 @@
 class Point:
-    color = "red"
-    circle = 2
+    MAX_COORD = 100
+    MIN_COORD = 0
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def set_coord(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __getattribute__(self, item):
+
+        return object.__getattribute__(self, item)
+
+    def __setattr__(self, key, value):
+        if key == "z":
+            raise AttributeError(
+                "Недопустимое имя атрибута")
+        else:
+            print("__SETATTR__")
+        # object.__setattr__(self, key, value)
+        self.__dict__[key] = value
+
+    def __getattr__(self, item):
+        print("__GETATTRIBUTE")
+        return False
+
+    def __delattr__(self, item):
+        print("__delattr__")
+        object.__delattr__(self, item)
 
 
-a = Point()
-b = Point()
-a.x = 1
-a.y = 2
+a = Point(5, 3)
+
+del a.x
 print(a.__dict__)
